cd9d6dd81a26aa09853c7a80340f2833b3b3a1df
Ambrosiussen <paul@ambrosiussen.com>
2022-12-29 00:56:29 +0100

    Update gcode_to_scara.py

--- gcode_to_scara.py
+++ gcode_to_scara.py
@@ -13,69 +13,34 @@ OUTPUT_NAME = r"H:\Personal\SCAHARA\square_CONVERTED.gcode"
 MAX_RADIUS = 460.0
 MAX_RHO = 1.0 # Needs to be between 0.0 and 1.0
 UNITS_PER_CIRCLE = 360.0
-LINE_SEGMENT_SIZE = 1
+LINE_SEGMENT_SIZE = 1.0
 CENTER_DRAWING = True
-SCARA = False
+SCARA = True
 SCALE_TO_FIT = True
 UNIFORM_SCALE = 1.0 
 DEFAULT_DRAWING_SPEED = 1000.0
 
 #####################################
-class Vertex():
-    """
-    Utility for storing and accessing x and y components of a Vector2 for a GCode vertex.
-    """
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def magnitude(self):
-        """
-        Compute magnitude of vertex position from origin.
-        """
-        return math.sqrt(self.x**2 + self.y**2)
-
-    def distance_to(self, other):
-        """
-        Compute distance between this vertex instance and another.
-        """
-        return math.sqrt((self.x - other.x)**2 + (self.y - other.y) **2)
-
-    def __sub__(self, other):
-        """
-        Implements - operator for this class.
-        """
-        return np.array([self.x - other.x, self.y - other.y])
-
-    def __isub__(self, other):
-        """
-        Implements -= operator for this class
-        """
-        self.x -= other.x
-        self.y -= other.y
-        return self
-
-    def __imul__(self, other):
-        """
-        Implements *= operator for this class.
-        """
-        self.x *= other
-        self.y *= other
-        return self
-
+_last_succesful_vertex = np.array([0.0,0.0])
 def get_vertex_from_g01_command(command):
     """
-    Creates a Vertex instance from a G01 GCode command.
+    Creates a Vector from a G01 GCode command.
     """
-    X = float(re.search(r"X\d+\.*\d*", command).group(0)[1:])
-    Y = float(re.search(r"Y\d+\.*\d*", command).group(0)[1:])
-    return Vertex(X, Y)
+    X = re.search(r"X\d+\.*\d*", command)
+    X = float(X.group(0)[1:]) if X else _last_succesful_vertex[0]
+
+    Y = re.search(r"Y\d+\.*\d*", command)
+    Y = float(Y.group(0)[1:]) if Y else _last_succesful_vertex[1]
+
+    _last_succesful_vertex[0] = X
+    _last_succesful_vertex[1] = Y
+    return np.array([X,Y])
 
 def get_samples_between_vertices(p1, p2, resample_distance):
     """
-    Returns a list of Vertex() instances which are linear samples between pt1 and pt2.
+    Returns a list of Vectors which are linear samples between pt1 and pt2.
     """
-    pt_distance = p1.distance_to(p2)
+    pt_distance = np.linalg.norm(p1 - p2)
 
     if abs(pt_distance) < 0.0001 or resample_distance > pt_distance or abs(resample_distance)<0.0001:
         return [p1]
@@ -83,16 +48,16 @@ def get_samples_between_vertices(p1, p2, resample_distance):
         step_x, step_y = (p2 - p1) * (resample_distance / pt_distance)
         
         if abs(step_x) < 0.0001:
-            y = np.arange(p1.y, p2.y, step_y)
-            x = np.zeros(len(y)) + p1.x
+            y = np.arange(p1[1], p2[1], step_y)
+            x = np.zeros(len(y)) + p1[0]
         elif abs(step_y) < 0.0001:
-            x = np.arange(p1.x, p2.x, step_x)
-            y = np.zeros(len(x)) + p1.y
+            x = np.arange(p1[0], p2[0], step_x)
+            y = np.zeros(len(x)) + p1[1]
         else:
-            x = np.arange(p1.x, p2.x, step_x)
-            y = np.arange(p1.y, p2.y, step_y)    
+            x = np.arange(p1[0], p2[0], step_x)
+            y = np.arange(p1[1], p2[1], step_y)    
 
-        return [Vertex(x, y[i]) for i, x in enumerate(x)]
+        return [np.array([x, y[i]]) for i, x in enumerate(x)]
 
 def scara_xy_from_thetarho(theta, rho):
     """
@@ -114,20 +79,20 @@ with open(INPUT_NAME, 'r', encoding="utf-8") as infile:
         if line.startswith("G1") or line.startswith("G01"):
             valid_commands.append(line)
             vertex = get_vertex_from_g01_command(line)
-            x_vals.append(vertex.x)
-            y_vals.append(vertex.y)
+            x_vals.append(vertex[0])
+            y_vals.append(vertex[1])
 
 # Computing the average position of all gcode commands.
 # We do this to center the drawing for SCARA machines with the center being 0,0
 # We also compute a scaling factor for scaling the drawing to fit MAX_RADIUS
-centroid = Vertex(sum(x_vals)/len(x_vals), sum(y_vals)/len(x_vals)) if CENTER_DRAWING else Vertex(0,0)
-_maxX = max(map(lambda x: x-centroid.x,x_vals, key=abs))
-_maxY = max(map(lambda y: y-centroid.y,y_vals, key=abs))
+centroid = np.array([sum(x_vals)/len(x_vals), sum(y_vals)/len(x_vals)]) if CENTER_DRAWING else np.array([0,0])
+_maxX = max(map(lambda x: abs(x-centroid[0]),x_vals))
+_maxY = max(map(lambda y: abs(y-centroid[1]),y_vals))
 autofit = (MAX_RADIUS/(max(_maxX, _maxY)+1.5)) if SCALE_TO_FIT else 1.0
 
 #TODO: Remove Houdini code
-node = hou.pwd() 
-geo = node.geometry()
+# node = hou.pwd() 
+# geo = node.geometry()
 
 # Generate Clean "Funky SCARA GCode" with resampled points between original GCode Positions.
 with open(OUTPUT_NAME, 'w', encoding="utf-8") as outfile:
@@ -171,7 +136,7 @@ with open(OUTPUT_NAME, 'w', encoding="utf-8") as outfile:
 
                 # Compute the clockwise angle relative to Vector(1,0) around the origin.
                 # Theta gets offset to ensure shortest path in one direction.
-                rawTheta = math.atan2(vertex.x, vertex.y)
+                rawTheta = math.atan2(vertex[0], vertex[1])
                 rawTheta = (rawTheta + 2.0 * math.pi) % (2.0 * math.pi)
                 deltaTheta = rawTheta - _previousRawTheta
                 if deltaTheta < -math.pi:
@@ -187,12 +152,12 @@ with open(OUTPUT_NAME, 'w', encoding="utf-8") as outfile:
 
                 x,y = scara_xy_from_thetarho(theta, rho)
             else:
-                x = vertex.x
-                y = vertex.y
+                x = vertex[0]
+                y = vertex[1]
 
             # TODO: Remove Houdini Code
-            pt = geo.createPoint()
-            pt.setPosition(hou.Vector3(x, y, 0))
+            # pt = geo.createPoint()
+            # pt.setPosition(hou.Vector3(x, y, 0))
 
             line_command = f"G01 X{x:.3f} Y{y:.3f} Z0.0"
 
